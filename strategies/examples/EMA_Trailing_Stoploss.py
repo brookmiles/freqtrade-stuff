7fc698efdf0cf5bf857a9bd670403fdb042d0b31
Brook Miles <brookmiles@users.noreply.github.com>
2021-04-13 13:28:11 +0900

    examples of exploiting trailing stop in backtesting

---
+++ strategies/examples/EMA_Trailing_Stoploss.py
@@ -0,0 +1,35 @@
+from freqtrade.strategy import IStrategy
+from typing import Dict, List
+from pandas import DataFrame
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+
+class EMA_Trailing_Stoploss(IStrategy):
+
+    minimal_roi = { "0": 0.01 }
+
+    stoploss = -0.01
+    
+    trailing_stop = True
+    trailing_stop_positive = 0.001
+
+    timeframe = '1h'
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        dataframe['ema3'] = ta.EMA(dataframe, timeperiod=3)
+        dataframe['ema5'] = ta.EMA(dataframe, timeperiod=5)
+        dataframe['go_long'] = qtpylib.crossed_above(dataframe['ema3'], dataframe['ema5']).astype('int')
+
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            qtpylib.crossed_above(dataframe['go_long'], 0)
+        ,
+        'buy'] = 1
+
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        return dataframe
---
+++ strategies/examples/Magic_Trailing_Stoploss.py
@@ -0,0 +1,24 @@
+from freqtrade.strategy.interface import IStrategy
+from typing import Dict, List
+from pandas import DataFrame
+
+class Magic_Trailing_Stoploss(IStrategy):
+
+    minimal_roi = { "0": 0.01 }
+
+    stoploss = -0.01
+    
+    trailing_stop = True
+    trailing_stop_positive = 0.001
+
+    timeframe = '1h'
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['buy'] = 1
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        return dataframe
